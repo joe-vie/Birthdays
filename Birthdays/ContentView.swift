34a4222da10d45035e79620520c75706a8caf322
Jovie <marcemg101@gmail.com>
2025-08-08 10:49:06 -0400

    added ui onto app

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend (name: "Joan", birthday: .now),
+        Friend (name: "Maya", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            } // end of list
+            .navigationTitle("Birthdays")
             
-            
-        }
-        .padding()
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    // makes it so you can't put a date in the future
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name",text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                } // end of VStack
+                .padding()
+                .background(.bar)
+            }
+        } // end of navigation stack
     }
 }
 
